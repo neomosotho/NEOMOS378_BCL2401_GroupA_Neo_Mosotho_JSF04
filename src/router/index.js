698d2f528a62d3c99130f6c36e36ebde3d085f41
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-08-06 09:42:55 +0200

    "Added router to main.js and created router configuration in router/index.js"

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import ProductDetails from '../views/ProductDetails.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/product/:id',
+    name: 'ProductDetails',
+    component: ProductDetails
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
