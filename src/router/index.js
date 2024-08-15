65f5adca66985e4cbb754d95cbd94c66d6b25063
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-08-15 09:57:01 +0200

    "Added login route and authentication check in router"

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,25 @@ import ProductDetails from '../views/ProductDetails.vue';
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/product/:id', component: ProductDetails, props: true }
+    { path: '/product/:id', component: ProductDetails, props: true },
+    {   path: '/login',
+        name: 'Login',
+        component: Login }
 ];
   
 const router = createRouter({
     history: createWebHistory(),
     routes
 });
+
+router.beforeEach((to, from, next) => {
+    const isLoggedIn = !!localStorage.getItem('token')
+    
+    if (to.meta.requiresAuth && !isLoggedIn) {
+      next({ name: 'Login', query: { redirect: to.fullPath } })
+    } else {
+      next()
+    }
+  });
   
 export default router;
